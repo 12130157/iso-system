1d0a5a0adb12493ce351d9045430f6e267859d61
toantrt <toantrt@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-02-18 07:08:17 +0000

    SMS project framwork

---
+++ SMS/Source/SMS/SMS/common/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SMS.common
+{
+    class Constants
+    {
+        public static ThanhVienModel USER_LOGIN;
+        public static ArrayList MENU_LIST;
+    }
+}
---
+++ SMS/Source/SMS/SMS/view/form_Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SMS.Form_Menu
+{
+    public partial class form_Login : Form
+    {
+        public form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void butCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void butlogin_Click(object sender, EventArgs e)
+        {
+            new form_Main().Show();
+        }
+
+        
+    }
+}
