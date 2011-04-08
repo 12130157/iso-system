f1adedf4074b301704c0858c491f6979b0873689
ducthienkhtn <ducthienkhtn@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-04-08 06:53:21 +0000

    UPDATE PACKAGE PILOT

--- PackageISO/v.1.1.3/storeISO.sql
+++ PackageISO/v.1.1.3/storeISO.sql
@@ -2147,6 +2147,8 @@ if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[sp_iso_Fin
 	drop procedure [dbo].[sp_iso_FindGiaoAn]
 GO
 CREATE PROCEDURE sp_iso_FindGiaoAn
+	@IndexPage varchar(10),
+	@LengthPage varchar(10),
 	@MaNguoiTao varchar(10),
 	@MaNamHoc varchar(10),
 	@MaLop varchar(10),
@@ -2155,9 +2157,17 @@ CREATE PROCEDURE sp_iso_FindGiaoAn
 	@TinhTrang varchar(10),
 	@NgayTimBD varchar(11),
 	@NgayTimKT varchar(11),
-	@MaBoPhan varchar(5)
+	@MaBoPhan varchar(5),
+	@MaKhoa  varchar(5)
 AS
 BEGIN			
+
+	IF @IndexPage = '' OR  ISNULL(@IndexPage,'')=''
+	BEGIN
+		SET @IndexPage='1'
+	END
+	
+
 	DECLARE @DieuKienMaNguoiTao varchar(100)
 	DECLARE @DieuKienMaLop varchar(100)
 	DECLARE @DieuKienMaMonHoc varchar(100)
@@ -2176,13 +2186,19 @@ BEGIN
 	SET @DieuKienMaNamHoc=''
 	SET @DieuKienTimNgay=''
 
-	IF @MaBoPhan = 0 OR @MaBoPhan = 1 OR @MaBoPhan=16 OR @MaBoPhan=4 OR @MaBoPhan=2
+	IF (@MaBoPhan = 0 OR @MaBoPhan = 1 OR @MaBoPhan=16 OR @MaBoPhan=4 OR @MaBoPhan=2) AND @MaKhoa = ''
 	BEGIN
 		SET @DieuKienMaBoPhan=''
 	END
 	ELSE
 	BEGIN
-		SET @DieuKienMaBoPhan=' AND E.Ma_bo_phan='+@MaBoPhan
+		IF @MaKhoa = ''
+		BEGIN
+			SET @DieuKienMaBoPhan=' AND E.Ma_bo_phan='+@MaBoPhan
+		END
+		BEGIN
+			 SET @DieuKienMaBoPhan=' AND E.Ma_bo_phan='+@MaKhoa
+		END 
 	END
 
 		
@@ -2237,6 +2253,7 @@ SET @sql=' SELECT U.Co_Hieu As CoHieu,U.ID As MaCTKHGD,A.ID As MaKHGD,A.Ma_Nguoi
 		,B.NgayGui As NgayGui,B.Ngay_duyet As NgayDuyet,B.Ma_nguoi_duyet As MaNguoiDuyet
 ,ISNULL(K.Ho+'' ''+'' ''+K.Ten_lot+'' ''+K.Ten,'''') As NguoiDuyet
 		,C.Ten_Mon_Hoc As TenMonHoc,D.Ki_hieu As KiHieu,F.Ho+'' ''+'' ''+F.Ten_lot+'' ''+F.Ten As NguoiTao
+INTO #TEMP 
 FROM KeHoachGiangDay As A
 INNER JOIN GiaoAn As B
 ON 1=1 '     
@@ -2263,8 +2280,14 @@ ON G.Ten_DN=K.Ten_dang_nhap
 INNER JOIN ChiTietKHGD As U
 On U.Ma_giao_an=B.ID '
 +@DieuKienTimNgay +
-' ORDER BY A.Ma_nguoi_tao DESC,A.Ma_mon_hoc DESC, A.Ma_lop DESC '
-	
+' ORDER BY A.Ma_nguoi_tao DESC,A.Ma_mon_hoc DESC, A.Ma_lop DESC 
+
+SELECT * FROM (SELECT 
+(SELECT COUNT(*) FROM #TEMP AS A WHERE A.MaGiaoAn<= B.MaGiaoAn) AS [Index],*  
+FROM #TEMP  AS B) AS C
+WHERE C.[Index] BETWEEN '+@IndexPage+' AND '+CAST((CAST(@LengthPage AS INT)+CAST(@IndexPage AS INT)-1)AS VARCHAR) +' ORDER BY [Index] ASC '
+
+
 exec sp_executesql @sql
 END
 
@@ -2318,7 +2341,8 @@ CREATE PROCEDURE sp_iso_FindKeHoachGiangDay
  @TinhTrang varchar(10),  
  @NgayTimBD varchar(11),  
  @NgayTimKT varchar(11),  
- @MaBoPhan varchar(5)  
+ @MaBoPhan varchar(5),
+ @MaKhoa varchar(5)  
 AS  
 BEGIN     
  DECLARE @DieuKienMaNguoiTao varchar(100)  
@@ -2328,7 +2352,7 @@ BEGIN
  DECLARE @DieuKienHocKi varchar(100)  
  DECLARE @DieuKienMaNamHoc varchar(100)  
  DECLARE @DieuKienTimNgay varchar(100)  
- DECLARE @DieuKienMaBoPhan varchar(100)  
+ DECLARE @DieuKienMaBoPhan varchar(100) 
   
  SET @DieuKienMaNguoiTao=''  
  SET @DieuKienMaLop=''  
@@ -2337,15 +2361,23 @@ BEGIN
  SET @DieuKienHocKi=''  
  SET @DieuKienMaNamHoc=''  
  SET @DieuKienTimNgay=''  
-  
- IF  @MaBoPhan = 0 OR @MaBoPhan = 1 OR @MaBoPhan=16 OR @MaBoPhan=4 OR @MaBoPhan=2
+ 
+ IF (@MaBoPhan = 0 OR @MaBoPhan = 1 OR @MaBoPhan=16 OR @MaBoPhan=4 OR @MaBoPhan=2 ) AND @MaKhoa = ''
  BEGIN  
   SET @DieuKienMaBoPhan=''  
  END  
  ELSE  
  BEGIN  
-  SET @DieuKienMaBoPhan=' AND T1.Ma_bo_phan='+@MaBoPhan  
- END  
+	IF @MaKhoa = ''
+	BEGIN
+	 SET @DieuKienMaBoPhan=' AND T1.Ma_bo_phan='+@MaBoPhan
+	END
+	ELSE
+	BEGIN
+	 SET @DieuKienMaBoPhan=' AND T1.Ma_bo_phan='+@MaKhoa
+	END  
+ END 
+ 
   
  IF @NgayTimBD = ''  
  BEGIN  
@@ -4050,6 +4082,150 @@ END
 
 
 
+GO
+
+--sp_iso_GetLengthOfFindGiaoAn.sql
+if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].sp_iso_GetLengthOfFindGiaoAn') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+	drop procedure [dbo].sp_iso_GetLengthOfFindGiaoAn
+GO
+CREATE PROCEDURE sp_iso_GetLengthOfFindGiaoAn
+	@MaNguoiTao varchar(10),
+	@MaNamHoc varchar(10),
+	@MaLop varchar(10),
+	@MaMonHoc varchar(10),
+	@HocKi varchar(10),
+	@TinhTrang varchar(10),
+	@NgayTimBD varchar(11),
+	@NgayTimKT varchar(11),
+	@MaBoPhan varchar(5),
+	@MaKhoa varchar(5)
+AS
+BEGIN			
+	DECLARE @DieuKienMaNguoiTao varchar(100)
+	DECLARE @DieuKienMaLop varchar(100)
+	DECLARE @DieuKienMaMonHoc varchar(100)
+	DECLARE @DieuKienTinhTrang varchar(100)
+	DECLARE @DieuKienHocKi varchar(100)
+	DECLARE @DieuKienMaNamHoc varchar(100)
+	DECLARE @DieuKienTimNgay varchar(100)
+	DECLARE @DieuKienMaBoPhan varchar(100)
+
+
+	SET @DieuKienMaNguoiTao=''
+	SET @DieuKienMaLop=''
+	SET @DieuKienMaMonHoc=''
+	SET @DieuKienTinhTrang=''
+	SET @DieuKienHocKi=''
+	SET @DieuKienMaNamHoc=''
+	SET @DieuKienTimNgay=''
+
+	IF (@MaBoPhan = 0 OR @MaBoPhan = 1 OR @MaBoPhan=16 OR @MaBoPhan=4 OR @MaBoPhan=2)  AND @MaKhoa = ''
+	BEGIN
+		SET @DieuKienMaBoPhan=''
+	END
+	ELSE
+	BEGIN
+		IF @MaKhoa = ''
+		BEGIN
+			SET @DieuKienMaBoPhan=' AND E.Ma_bo_phan='+@MaBoPhan
+		END
+		BEGIN
+			 SET @DieuKienMaBoPhan=' AND E.Ma_bo_phan='+@MaKhoa
+		END 
+	END
+
+		
+	IF @NgayTimBD = ''
+	BEGIN
+		SET @NgayTimBD='1/1/1'
+	END
+	
+	IF @NgayTimKT =''
+	BEGIN
+		SET @DieuKienTimNgay= ' WHERE U.Ngay_BD >= ''' +  @NgayTimBD+'''' 
+	END
+	ELSE
+	BEGIN
+		SET @DieuKienTimNgay= ' WHERE U.Ngay_BD >= ''' + @NgayTimBD  + ''' AND U.Ngay_BD <= ''' + @NgayTimKT +''''
+	END	
+
+	IF @MaNguoiTao <> ''
+	BEGIN
+		SET @DieuKienMaNguoiTao=' AND A.Ma_nguoi_tao = '+@MaNguoiTao
+	END
+	
+	IF @MaLop <> ''
+	BEGIN
+		SET @DieuKienMaLop=' AND Ma_Lop = '+@MaLop
+	END
+	
+	IF @MaMonHoc <> ''
+	BEGIN
+		SET @DieuKienMaMonHoc =' AND Ma_mon_hoc ='+ @MaMonHoc
+	END
+
+	IF @TinhTrang <>''
+	BEGIN
+		SET @DieuKienTinhTrang =' AND B.Tinh_trang = '+@TinhTrang 
+	END
+
+	IF @HocKi <>''
+	BEGIN
+		SET @DieuKienHocKi=' AND Hoc_ki = '+@HocKi
+	END	
+	IF @MaNamHoc <>''
+	BEGIN
+		SET @DieuKienMaNamHoc=' AND Ma_nam_hoc  = '+ @MaNamHoc
+	END
+	
+	DECLARE @sql nvarchar(2000)
+
+
+SET @sql=' SELECT U.Co_Hieu As CoHieu,U.ID As MaCTKHGD,A.ID As MaKHGD,A.Ma_Nguoi_Tao As MaNguoiTao,A.Ma_mon_hoc As MaMonHoc,U.Ngay_BD As NgayThucHien
+		,A.Ma_lop As MaLop,B.ID As MaGiaoAn,B.Tinh_Trang As TinhTrang,B.So_Giao_An As SoGiaoAn 
+		,B.NgayGui As NgayGui,B.Ngay_duyet As NgayDuyet,B.Ma_nguoi_duyet As MaNguoiDuyet
+,ISNULL(K.Ho+'' ''+'' ''+K.Ten_lot+'' ''+K.Ten,'''') As NguoiDuyet
+		,C.Ten_Mon_Hoc As TenMonHoc,D.Ki_hieu As KiHieu,F.Ho+'' ''+'' ''+F.Ten_lot+'' ''+F.Ten As NguoiTao
+INTO #TEMP 
+FROM KeHoachGiangDay As A
+INNER JOIN GiaoAn As B
+ON 1=1 '     
+			 +@DieuKienMaNguoiTao  
+			 +@DieuKienHocKi
+			 +@DieuKienMaNamHoc
+			 +@DieuKienMaLop
+			 +@DieuKienMaMonHoc 
+			 +' AND B.Ma_KHGD=A.ID ' 
+			 +@DieuKienTinhTrang
+			 
++' INNER JOIN MonHoc As C
+ON C.ID=A.Ma_Mon_Hoc
+INNER JOIN LopHoc As D
+ON D.ID=A.Ma_Lop 
+INNER JOIN ThanhVien As E
+On E.ID=A.Ma_Giao_Vien '+ @DieuKienMaBoPhan +'
+INNER JOIN ChiTietThanhVien As F
+ON E.Ten_DN=F.Ten_dang_nhap
+LEFT JOIN ThanhVien As G
+On G.ID=B.Ma_nguoi_duyet
+LEFT JOIN ChiTietThanhVien As K
+ON G.Ten_DN=K.Ten_dang_nhap
+INNER JOIN ChiTietKHGD As U
+On U.Ma_giao_an=B.ID '
++@DieuKienTimNgay +
+' ORDER BY A.Ma_nguoi_tao DESC,A.Ma_mon_hoc DESC, A.Ma_lop DESC 
+
+SELECT COUNT(*) AS TOTAL FROM #TEMP
+
+'
+
+
+	
+exec sp_executesql @sql
+END
+
+
+	
 GO
 
 --sp_ISO_GetLichSuDungPhong.sql
