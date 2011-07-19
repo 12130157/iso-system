8a8903f014439960188e1c72dbc567dfb2239053
ducthienkhtn <ducthienkhtn@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-07-19 06:22:14 +0000

    add project IN THI TOT NGHIEP

---
+++ Chuong Trinh Thi Tot Nghiep/chuong trinh in thay minh/bailam/nhungfileexcel/nhungfileexcel/frmrpttonghop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace nhungfileexcel
+{
+    public partial class frmrpttonghop : Form
+    {
+        public frmrpttonghop()
+        {
+            InitializeComponent();
+        }
+
+        private void frmrpttonghop_Load(object sender, EventArgs e)
+        {
+            rpttonghop rpt = new rpttonghop();
+            //rpt.SetDataSource(dts.Tables[0]);
+            //CrystalDecisions.Shared.TableLogOnInfo dd=rpt.Database.Tables[0].LogOnInfo;
+            //dd.ConnectionInfo.DatabaseName = Path.Combine(Directory.GetCurrentDirectory(), "bangdiem.mdb");
+            //dd.TableName = "tblrptbailam";
+            //rpt.Database.Tables[0].ApplyLogOnInfo(dd);
+            //rpt.VerifyDatabase();
+            //Data tbl=rpt.Database.Tables["tblrptbailam"].LogOnInfo();
+            rpt.Refresh();
+            cv.ReportSource = rpt;
+        }
+    }
+}
