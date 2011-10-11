5127275a307e8d735b2168128ae12f010eb5aa34
thanhtc.hv@gmail.com <thanhtc.hv@gmail.com@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-10-11 09:18:45 +0000

    up

---
+++ trunk/ISOAutoEmailService/src/ISOAutoEmailService.java
@@ -0,0 +1,14 @@
+import view.AutoEmailService;
+
+
+public class ISOAutoEmailService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new AutoEmailService();
+	}
+
+}
---
+++ trunk/ISOAutoEmailService/src/util/DataUtil.java
@@ -0,0 +1,141 @@
+package util;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * Containing methods to manipulate database.
+ * @author ThienVD
+ *
+ */
+public class DataUtil {
+
+	static Connection conn = null;
+	
+	public static String URL = "jdbc:sqlserver://COLDER_TRAN;databaseName=hungvuongsystem";
+	public static String USER_NAME = "sa";
+	public static String PASSWORD = "123456";
+
+	/**
+	 * Create connection to database.
+	 */
+	public static void connect() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Disconnect sql connection.
+	 */
+	public static void disconnected() {
+		try {
+			if (conn != null) {
+				conn.close();
+				conn = null;
+			}
+
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Get connection.
+	 * @return Connection
+	 */
+	public static Connection getConnection() {
+		if (conn == null)
+			connect();
+		return conn;
+	}
+
+	/**
+	 * Check connection.
+	 * @return int
+	 */
+	public static int checkConnection() {
+		try {
+			if (conn != null) {
+				return 0;
+			}
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
+		return 1;
+	}
+
+	/**
+	 *
+	 * @param sql
+	 * @return ResultSet
+	 */
+	public static ResultSet executeQuery(String sql) {
+		try {
+			connect();
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			return rs;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 *
+	 * @param sql
+	 * @return int
+	 */
+	public static int executeUpdate(String sql) {
+		try {
+			connect();
+			Statement statement = conn.createStatement();
+			int rsUpdate = statement.executeUpdate(sql);
+			return rsUpdate;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	/**
+	 *
+	 * @param csmt
+	 * @return ResultSet
+	 */
+	public static ResultSet executeStore(CallableStatement csmt) {
+		try {
+			connect();
+			return csmt.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 *
+	 * @param csmt
+	 * @return int
+	 */
+	public static Boolean executeNonStore(CallableStatement csmt) {
+		try {
+			connect();
+			csmt.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}				
+	}
+
+}
