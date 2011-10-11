b09ef7c5ddbc3dfd7b60edc3343c1ecc4f0a3448
thanhtc.hv@gmail.com <thanhtc.hv@gmail.com@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-10-11 09:16:25 +0000

    up

--- trunk/ISO/db/stored/sp_ISO_GetGiaoAnDelayList.sql
+++ trunk/ISO/db/stored/sp_ISO_GetGiaoAnDelayList.sql
@@ -20,8 +20,10 @@ BEGIN
 		Ma_mon_hoc			int,
 		Ten_mon_hoc			nvarchar(50),
 		Giao_an_so			int,
-		Ngay_day			datetime,
-		Nhom				int
+		Ngay_day			varchar(10),
+		Ngay_gui			varchar(10),
+		Nhom				int,
+		Co_hieu				int
 	)
 	
 	insert into GiaoAnDelayList
@@ -35,21 +37,50 @@ BEGIN
 		A.Ma_mon_hoc,
 		F.Ten_mon_hoc,
 		B.STT_noi_dung as 'Giao_an_so',
-		B.Ngay_BD as 'Ngay_day',
-		B.Nhom as 'Nhom'				
+		CONVERT(VARCHAR(10),B.Ngay_BD,105) as 'Ngay_day',
+		'' as 'Ngay_gui',
+		B.Nhom as 'Nhom',
+		'1' as 'Co_hieu'		
 	from KeHoachGiangDay A
 	inner join ChiTietKHGD B on A.ID=B.Ma_ke_hoach_giang_day
 	inner join ThanhVien C on A.Ma_giao_vien=C.ID
 	inner join ChiTietThanhVien D on C.Ten_DN=D.Ten_dang_nhap
 	inner join LopHoc E on A.Ma_lop=E.ID
 	inner join MonHoc F on A.Ma_mon_hoc=F.ID
-	where Ma_ke_hoach_giang_day=1
-	and ISNULL(B.Ma_giao_an,'')=''
+	where ISNULL(B.Ma_giao_an,'')=''
 	and DATEDIFF(day, getdate(),B.Ngay_BD) >=0 
-	and DATEDIFF(day, getdate(),B.Ngay_BD) <= @Day
-	and ISNULL(B.User1,'') = ''		
-	
+	and DATEDIFF(day, getdate(),B.Ngay_BD) <= @Day	
+	order by B.Ngay_BD
+
+	insert into GiaoAnDelayList
+	select
+		B.ID AS 'id_ChiTietKHGD', 
+		A.Ma_giao_vien,
+		D.Ho + ' ' + D.Ten_lot + ' ' + D.Ten as 'Ho_ten_giao_vien',
+		D.Email,
+		A.Ma_lop,
+		E.Ki_hieu as 'Ten_lop',
+		A.Ma_mon_hoc,
+		F.Ten_mon_hoc,
+		B.STT_noi_dung as 'Giao_an_so',
+		CONVERT(VARCHAR(10),B.Ngay_BD,105) as 'Ngay_day',
+		ISNULL(CONVERT(VARCHAR(10),G.NgayGui,105),'') as 'Ngay_gui',
+		B.Nhom as 'Nhom',
+		'2' as 'Co_hieu'	
+	from KeHoachGiangDay A
+	inner join ChiTietKHGD B on A.ID=B.Ma_ke_hoach_giang_day
+	inner join ThanhVien C on A.Ma_giao_vien=C.ID
+	inner join ChiTietThanhVien D on C.Ten_DN=D.Ten_dang_nhap
+	inner join LopHoc E on A.Ma_lop=E.ID
+	inner join MonHoc F on A.Ma_mon_hoc=F.ID
+	left join GiaoAn G on B.Ma_giao_an=G.id
+	where (ISNULL(B.Ma_giao_an,'')='' or  (SELECT NgayGui FROM GIAOAN WHERE ID=B.Ma_giao_an) > B.Ngay_BD)
+	and getdate() > B.Ngay_BD
+	and A.Ma_nam_hoc = (SELECT ID FROM NAMHOC WHERE Nam_bat_dau=DATEPART(YEAR,GETDATE()))
+	order by B.Ngay_BD
+
 	-- UPDATE COLUMN USER1 TRONG TABLE ChiTietKHGD THANH 1, CO HIEU LA DA EMAIL NHAC NHO GIAOAN DELAY ROI	
+	/*
 	DECLARE @ID_ChiTietKHGD INT		
 	DECLARE @C CURSOR		
 	SET @C = CURSOR FOR SELECT id_ChiTietKHGD FROM GiaoAnDelayList
@@ -65,7 +96,7 @@ BEGIN
 		
 		FETCH NEXT FROM @C INTO @ID_ChiTietKHGD
 	END
-	/*
+	
 	-- LAY DU LIEU TRA VE DE SEND EMAIL
 	SELECT
 		
