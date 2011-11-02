1013d8528e6194987ed8710af49e5668a18e4ff7
thanhtc.hv@gmail.com <thanhtc.hv@gmail.com@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-11-02 09:42:07 +0000

    fix Cập Nhật Thông Tin

--- trunk/ISO/db/stored/sp_NhanSu_CapNhatThongTin.sql
+++ trunk/ISO/db/stored/sp_NhanSu_CapNhatThongTin.sql
@@ -23,6 +23,8 @@ CREATE PROC sp_NhanSu_CapNhatThongTin
 @Dien_thoai_dd VARCHAR(20),
 @Ngay_thu_viec DATETIME,
 @Ngay_vao_lam DATETIME,
+@Vai_tro INT,
+@Bo_phan INT,
 @KQ	INT OUTPUT
 AS
 BEGIN
@@ -44,25 +46,53 @@ BEGIN
 		
 		IF NOT EXISTS (SELECT * FROM DENGHIKTV WHERE Nguoi_du_tuyen=@KQ)
 		BEGIN
+			IF @Ngay_thu_viec = 0
+			BEGIN
+				set @Ngay_thu_viec = NULL
+			END
 			INSERT INTO DENGHIKTV(Nguoi_du_tuyen,Bat_dau,Tinh_trang,Ngay_cap_nhat_cuoi) VALUES(@KQ,@Ngay_thu_viec,'-1',GETDATE())
 		END
 		ELSE
 		BEGIN
+			IF @Ngay_thu_viec = 0
+			BEGIN
+				set @Ngay_thu_viec = NULL
+			END
 			UPDATE DENGHIKTV SET Bat_dau=@Ngay_thu_viec,Ngay_cap_nhat_cuoi=GETDATE() WHERE Nguoi_du_tuyen=@KQ
 		END
 		
 		IF NOT EXISTS (SELECT * FROM HOPDONGLAODONG WHERE Nguoi_du_tuyen=@KQ)
 		BEGIN
+			IF @Ngay_vao_lam = 0
+			BEGIN
+				set @Ngay_vao_lam = NULL
+			END
 			INSERT INTO HOPDONGLAODONG(Nguoi_du_tuyen,Bat_dau,Tinh_trang,Ngay_cap_nhat_cuoi) VALUES(@KQ,@Ngay_vao_lam,'-1',GETDATE())
 		END
 		ELSE
 		BEGIN
+			IF @Ngay_vao_lam = 0
+			BEGIN
+				set @Ngay_vao_lam = NULL
+			END
 			UPDATE HOPDONGLAODONG SET Bat_dau=@Ngay_vao_lam,Ngay_cap_nhat_cuoi=GETDATE() WHERE Nguoi_du_tuyen=@KQ
 		END
 		
-		SELECT @KQ = Ma_dia_chi FROM CHITIETTHANHVIEN WHERE Ten_dang_nhap=@Ten_dang_nhap
+		SELECT @KQ = ISNULL(Ma_dia_chi,'-1') FROM CHITIETTHANHVIEN WHERE Ten_dang_nhap=@Ten_dang_nhap
 		
-		UPDATE DIACHI SET So_nha=@So_nha,Duong=@Duong,Phuong_xa=@Phuong_xa,Quan_huyen=@Quan_huyen,Tinh_thanhpho=@Tinh_thanhpho,Dien_thoai_nha=@Dien_thoai_nha,Lan_cap_nhat_cuoi=GETDATE() WHERE ID=@KQ
+		IF @KQ = -1
+		BEGIN
+			INSERT INTO DIACHI(So_nha,Duong,Phuong_xa,Quan_huyen,Tinh_thanhpho,Dien_thoai_nha,Lan_cap_nhat_cuoi) VALUES (@So_nha,@Duong,@Phuong_xa,@Quan_huyen,@Tinh_thanhpho,@Dien_thoai_nha,GETDATE())
+			SELECT @KQ=MAX(ID) FROM DIACHI
+			UPDATE CHITIETTHANHVIEN SET Ma_dia_chi=@KQ WHERE Ten_dang_nhap=@Ten_dang_nhap
+		END
+		ELSE
+		BEGIN
+			UPDATE DIACHI SET So_nha=@So_nha,Duong=@Duong,Phuong_xa=@Phuong_xa,Quan_huyen=@Quan_huyen,Tinh_thanhpho=@Tinh_thanhpho,Dien_thoai_nha=@Dien_thoai_nha,Lan_cap_nhat_cuoi=GETDATE() WHERE ID=@KQ
+		END
+		SELECT * FROM THANHVIEN		
+
+		UPDATE THANHVIEN SET Ma_vai_tro=@Vai_tro,Ma_bo_phan=@Bo_phan WHERE Ten_DN=@Ten_dang_nhap
 		UPDATE CHITIETTHANHVIEN SET Ho=@Ho,Ten_lot=@Ten_lot,Ten=@Ten,Ngay_sinh=@Ngay_sinh,Email=@Email,Dien_thoai_dd=@Dien_thoai_dd,Chung_minh_nhan_dan=@CMND,Ngay_cap=@Ngay_cap,Noi_cap=@Noi_cap WHERE Ten_dang_nhap=@Ten_dang_nhap
 		SET @KQ = @ID
 	END
