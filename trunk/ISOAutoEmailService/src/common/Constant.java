5edf004c130f11943ca61c3da26cd6358351bfb8
thanhtc.hv@gmail.com <thanhtc.hv@gmail.com@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-11-13 01:28:17 +0000

    up

---
+++ trunk/ISOAutoEmailService/src/common/Constant.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.util.ResourceBundle;
+
+import javax.swing.DefaultListModel;
+
+import controller.ThongBao;
+
+public class Constant {
+	public static ResourceBundle SETTING_RES = null;
+	public static ThongBao thongBao = null;
+	public static DefaultListModel model = null;
+}
