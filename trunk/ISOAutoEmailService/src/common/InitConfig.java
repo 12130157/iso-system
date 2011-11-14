08ed8d4f006d2026a50dfbdb57a10ee5578ea36a
thanhtc.hv@gmail.com <thanhtc.hv@gmail.com@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-11-14 06:36:45 +0000

    up

--- trunk/ISOAutoEmailService/src/common/InitConfig.java
+++ trunk/ISOAutoEmailService/src/common/InitConfig.java
@@ -14,6 +14,23 @@ public class InitConfig {
 		Constant.SETTING_RES = ResourceBundle.getBundle("setting");
 		Constant.model = new DefaultListModel();
 		Constant.mailCC = new ArrayList<String>();
+		Constant.listGio = new String[24];
+		Constant.listPhut = new String[60];
+		Constant.listThu = new String[7];
+		for (int i = 0; i < 60; i++) {
+			Constant.listPhut[i] = i+"";
+		}
+		for (int i = 0; i < 24; i++) {
+			Constant.listGio[i] = i+"";
+		}
+		Constant.listThu[0] = "Chủ Nhật";
+		Constant.listThu[1] = "Thứ 2";
+		Constant.listThu[2] = "Thứ 3";
+		Constant.listThu[3] = "Thứ 4";
+		Constant.listThu[4] = "Thứ 5";
+		Constant.listThu[5] = "Thứ 6";
+		Constant.listThu[6] = "Thứ 7";
+		
 		String [] n = Constant.SETTING_RES.getString("mailCC").split(";");
 		for (String mail : n) {
 			Constant.mailCC.add(mail);
