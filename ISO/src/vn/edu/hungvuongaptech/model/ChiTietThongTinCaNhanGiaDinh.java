5628e37949732ebac471d52696f14025241ec69e
qchuong100 <qchuong100@69450bdd-8c94-ae96-153c-8b270b4a2f79>
2011-11-23 09:08:41 +0000

    up ChiTietThongTinCaNhanGiaDinh model

---
+++ ISO/src/vn/edu/hungvuongaptech/model/ChiTietThongTinCaNhanGiaDinh.java
@@ -0,0 +1,233 @@
+package vn.edu.hungvuongaptech.model;
+
+public class ChiTietThongTinCaNhanGiaDinh {
+	public String TenDangNhap;
+	public String GioiTinh;
+	public String TenKhaiSinh;
+	public String TenThuongGoi;
+	public String NgaySinh;
+	public String NoiSinh;
+	public String QueQuan;
+	public String NoiDangKyThuongTru;
+	public String DanToc;
+	public String TonGiao;
+	public String TrinhDoHocVanTruocKhiVaoHoc;
+	public String NgayThamGiaDangCSVN;
+	public String NgayChinhThuc;
+	public String NgayKetNapDoanTNCS;
+	public String HoTenBo;
+	public String NgheNghiepBo;
+	public String HoTenMe;
+	public String NgheNghiepMe;
+	public String HoTenVoChong;
+	public String NgheNghiepVoChong;
+	public String DoiTuongThuocDienChinhSach;
+	public String NgheNghiepLamTruocKhiVaoHoc;
+	public String DiaChi;
+	public String DTDD;
+	public String NguyenVong;
+
+	public ChiTietThongTinCaNhanGiaDinh() {
+	}
+	
+	public String getTenDangNhap() {
+		return TenDangNhap;
+	}
+
+	public void setTenDangNhap(String tenDangNhap) {
+		TenDangNhap = tenDangNhap;
+	}
+
+	public String getGioiTinh() {
+		return GioiTinh;
+	}
+
+	public void setGioiTinh(String gioiTinh) {
+		GioiTinh = gioiTinh;
+	}
+
+	public String getTenKhaiSinh() {
+		return TenKhaiSinh;
+	}
+
+	public void setTenKhaiSinh(String tenKhaiSinh) {
+		TenKhaiSinh = tenKhaiSinh;
+	}
+
+	public String getTenThuongGoi() {
+		return TenThuongGoi;
+	}
+
+	public void setTenThuongGoi(String tenThuongGoi) {
+		TenThuongGoi = tenThuongGoi;
+	}
+
+	public String getNgaySinh() {
+		return NgaySinh;
+	}
+
+	public void setNgaySinh(String ngaySinh) {
+		NgaySinh = ngaySinh;
+	}
+
+	public String getNoiSinh() {
+		return NoiSinh;
+	}
+
+	public void setNoiSinh(String noiSinh) {
+		NoiSinh = noiSinh;
+	}
+
+	public String getQueQuan() {
+		return QueQuan;
+	}
+
+	public void setQueQuan(String queQuan) {
+		QueQuan = queQuan;
+	}
+
+	public String getNoiDangKyThuongTru() {
+		return NoiDangKyThuongTru;
+	}
+
+	public void setNoiDangKyThuongTru(String noiDangKyThuongTru) {
+		NoiDangKyThuongTru = noiDangKyThuongTru;
+	}
+
+	public String getDanToc() {
+		return DanToc;
+	}
+
+	public void setDanToc(String danToc) {
+		DanToc = danToc;
+	}
+
+	public String getTonGiao() {
+		return TonGiao;
+	}
+
+	public void setTonGiao(String tonGiao) {
+		TonGiao = tonGiao;
+	}
+
+	public String getTrinhDoHocVanTruocKhiVaoHoc() {
+		return TrinhDoHocVanTruocKhiVaoHoc;
+	}
+
+	public void setTrinhDoHocVanTruocKhiVaoHoc(String trinhDoHocVanTruocKhiVaoHoc) {
+		TrinhDoHocVanTruocKhiVaoHoc = trinhDoHocVanTruocKhiVaoHoc;
+	}
+
+	public String getNgayThamGiaDangCSVN() {
+		return NgayThamGiaDangCSVN;
+	}
+
+	public void setNgayThamGiaDangCSVN(String ngayThamGiaDangCSVN) {
+		NgayThamGiaDangCSVN = ngayThamGiaDangCSVN;
+	}
+
+	public String getNgayChinhThuc() {
+		return NgayChinhThuc;
+	}
+
+	public void setNgayChinhThuc(String ngayChinhThuc) {
+		NgayChinhThuc = ngayChinhThuc;
+	}
+
+	public String getNgayKetNapDoanTNCS() {
+		return NgayKetNapDoanTNCS;
+	}
+
+	public void setNgayKetNapDoanTNCS(String ngayKetNapDoanTNCS) {
+		NgayKetNapDoanTNCS = ngayKetNapDoanTNCS;
+	}
+
+	public String getHoTenBo() {
+		return HoTenBo;
+	}
+
+	public void setHoTenBo(String hoTenBo) {
+		HoTenBo = hoTenBo;
+	}
+
+	public String getNgheNghiepBo() {
+		return NgheNghiepBo;
+	}
+
+	public void setNgheNghiepBo(String ngheNghiepBo) {
+		NgheNghiepBo = ngheNghiepBo;
+	}
+
+	public String getHoTenMe() {
+		return HoTenMe;
+	}
+
+	public void setHoTenMe(String hoTenMe) {
+		HoTenMe = hoTenMe;
+	}
+
+	public String getNgheNghiepMe() {
+		return NgheNghiepMe;
+	}
+
+	public void setNgheNghiepMe(String ngheNghiepMe) {
+		NgheNghiepMe = ngheNghiepMe;
+	}
+
+	public String getHoTenVoChong() {
+		return HoTenVoChong;
+	}
+
+	public void setHoTenVoChong(String hoTenVoChong) {
+		HoTenVoChong = hoTenVoChong;
+	}
+
+	public String getNgheNghiepVoChong() {
+		return NgheNghiepVoChong;
+	}
+
+	public void setNgheNghiepVoChong(String ngheNghiepVoChong) {
+		NgheNghiepVoChong = ngheNghiepVoChong;
+	}
+
+	public String getDoiTuongThuocDienChinhSach() {
+		return DoiTuongThuocDienChinhSach;
+	}
+
+	public void setDoiTuongThuocDienChinhSach(String doiTuongThuocDienChinhSach) {
+		DoiTuongThuocDienChinhSach = doiTuongThuocDienChinhSach;
+	}
+
+	public String getNgheNghiepLamTruocKhiVaoHoc() {
+		return NgheNghiepLamTruocKhiVaoHoc;
+	}
+
+	public void setNgheNghiepLamTruocKhiVaoHoc(String ngheNghiepLamTruocKhiVaoHoc) {
+		NgheNghiepLamTruocKhiVaoHoc = ngheNghiepLamTruocKhiVaoHoc;
+	}
+
+	public String getDiaChi() {
+		return DiaChi;
+	}
+
+	public void setDiaChi(String diaChi) {
+		DiaChi = diaChi;
+	}
+
+	public String getDTDD() {
+		return DTDD;
+	}
+
+	public void setDTDD(String dTDD) {
+		DTDD = dTDD;
+	}
+
+	public String getNguyenVong() {
+		return NguyenVong;
+	}
+
+	public void setNguyenVong(String nguyenVong) {
+		NguyenVong = nguyenVong;
+	}
+
+}
